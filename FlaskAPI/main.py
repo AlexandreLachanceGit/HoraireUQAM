70e05b48155375065d443644c9e21cbc84523976
Lachance, Alexandre <lachance.alexandre.5@courrier.uqam.ca>
2021-09-30 12:27:17 -0400

    Updated 'requirements.txt'.

--- FlaskAPI/main.py
+++ FlaskAPI/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
 from CourseInfo import getCourse, getCourses
+from Schedule import getSchedule
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,9 +13,14 @@ class CourseInfo (Resource):
 class CoursesInfo (Resource):
     def get(self, courseCodes, trimesterCode):
         return jsonify(getCourses(courseCodes, trimesterCode))
+
+class Schedule (Resource):
+    def get(self, courseCodes, trimesterCode):
+        return jsonify(getSchedule(courseCodes, trimesterCode))
     
 api.add_resource(CourseInfo, "/getCourse/<string:courseCode>-<string:trimesterCode>")
 api.add_resource(CoursesInfo, "/getCourses/<string:courseCodes>-<string:trimesterCode>")
+api.add_resource(Schedule, "/getCourses/<string:courseCodes>-<string:trimesterCode>")
 
 
 if __name__ == "__main__":
